ac2519ddcae78310138b4c05ebf8221fa70793a4
abdelrahman.ebied <abdelrahman.ebied@itworx.com>
2024-12-14 17:32:16 +0200

    Initial Commit

---
+++ todoornottodo/todoornottodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  todoornottodoApp.swift
+//  todoornottodo
+//
+//  Created by Abdelrahman Ebied on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct todoornottodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ todoornottodoTests/todoornottodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoornottodoTests.swift
+//  todoornottodoTests
+//
+//  Created by Abdelrahman Ebied on 12/14/24.
+//
+
+import Testing
+@testable import todoornottodo
+
+struct todoornottodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
